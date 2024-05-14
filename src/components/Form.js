2a809ca52111ab0b76d4419320a10e552f4a04cc
Joel Rdz <joelrdzh@gmail.com>
2024-05-14 12:23:41 -0600

    Create input with React Native TextInput and Form styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,59 @@
 import React from 'react';
-import { Modal, Text } from 'react-native';
+import { Modal, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const Form = ({ modalVisible }) => {
   return (
     <Modal animationType="slide" visible={modalVisible}>
-      <Text>From Modal</Text>
+      <SafeAreaView style={styles.content}>
+        <Text style={styles.title}>
+          Nueva {''}
+          <Text style={styles.titleBold}>Cita</Text>
+        </Text>
+
+        <View style={styles.field}>
+          <Text style={styles.label}>Nombre Paciente</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nombre Paciente"
+            placeholderTextColor={'#666'}
+          />
+        </View>
+      </SafeAreaView>
     </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  content: {
+    backgroundColor: '#6D28D9',
+    flex: 1,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '600',
+    textAlign: 'center',
+    marginTop: 30,
+    color: '#FFF',
+  },
+  titleBold: {
+    fontWeight: '900',
+  },
+  field: {
+    marginTop: 10,
+    marginHorizontal: 30,
+  },
+  label: {
+    color: '#FFF',
+    marginBottom: 10,
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+  },
+});
 
 export default Form;
