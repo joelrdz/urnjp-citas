c9d9e7ffb8bdf74986d23fb3244d0e527fd7a2f3
Joel Rdz <joelrdzh@gmail.com>
2024-05-14 01:31:55 -0600

    App basic styles with StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import {
   SafeAreaView,
+  StyleSheet,
   Text,
 } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
         Administrador de Citas {''}
-        <Text>Veterinaria</Text>
+        <Text style={styles.titleBold}>Veterinaria</Text>
       </Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F&',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600',
+  },
+  titleBold: {
+    fontWeight: '900',
+    color: '#6D28D9',
+  },
+});
 
 export default App;
